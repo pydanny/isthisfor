5080c64510a6d2a2c98702ba48e11d9e20673f95
Audrey Roy <audreyr@gmail.com>
2012-02-10 18:46:29 -0800

    First pass at pitch list & detail views.

--- project/pitch/views.py
+++ project/pitch/views.py
@@ -2,14 +2,12 @@ from django.views.generic import DetailView, ListView
 
 from pitch.models import Pitch
 
-class PitchDetailView(DetailView):
-    
+class PitchListView(ListView):    
     queryset=Pitch.objects.order_by('-pub_date')
     context_object_name='latest_pitch_list'
     template_name='pitch/pitch_list.html'
     
-class PitchListView(ListView):
-
+class PitchDetailView(DetailView):
     model=Pitch
     template_name='pitch/pitch_detail.html'
     name='pitch_detail'
