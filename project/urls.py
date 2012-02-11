5f8442220587ef71a8457a8b18c208b7e03255ae
Daniel Greenfeld <pydanny@gmail.com>
2012-02-10 18:13:16 -0800

    adding pitch to urls

--- project/urls.py
+++ project/urls.py
@@ -5,7 +5,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
 
-    #url(r'^', include('pitch.urls')),
+    url(r'^', include('pitch.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 )
